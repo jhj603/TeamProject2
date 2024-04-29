41c27664e77496121d0b62df26f4856d2de1dbc0
Changil Cho <chotogi98@gmail.com>
2024-04-29 20:38:35 +0900

    update

--- TeamProject2/TeamProject2/GameObjects/Character/Monster/Monster.cs
+++ TeamProject2/TeamProject2/GameObjects/Character/Monster/Monster.cs
@@ -11,5 +11,17 @@ namespace TeamProject2
         // 요구조건을 보고 필요하다고 느끼는 필드를 추가해주세요.
 
         // Initialize() 함수를 만들어서 내부에서 필드 값 초기화를 할 수 있게 구현해주세요.
+
+        public int Lv { get; }
+        public string Name { get; }
+        public int Hp { get; set; }
+
+
+        public Monster(int lv, string name, int hp)
+        {
+            Lv = lv;
+            Name = name;
+            Hp = hp;
+        }
     }
 }
--- TeamProject2/TeamProject2/Scenes/BattleScene.cs
+++ TeamProject2/TeamProject2/Scenes/BattleScene.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using TeamProject2.GameObjects.Player;
 
 namespace TeamProject2
 {
-    internal class BattleScene
+    internal class BattleScene : Scene
     {
         List<Monster> monsters = new List<Monster>()
         {
@@ -17,7 +16,26 @@ namespace TeamProject2
             new Monster(3, "공허충", 10)
         };
 
-        public void Battle()
+        public override bool Initialize(Player playerInstance)
+        {
+            return base.Initialize(playerInstance);
+        }
+
+        public override SceneType Update()
+        {
+            PrintDialogue();
+
+            switch (MenuChoice("원하시는 행동을 입력해주세요.", 1, 1))
+            {
+                case 1:
+                    ChoiceMonster();
+                    break;
+            }
+
+            return SceneType.NoChange;
+        }
+
+        protected override void PrintDialogue()
         {
             Console.Clear();
 
@@ -26,63 +44,56 @@ namespace TeamProject2
             for (int i = 0; i < 3; i++)
             {
                 Monster randomMonster = GetRandomMonster(monsters);
-                Console.WriteLine ($"Lv. {randomMonster.Lv} {randomMonster.Name} Hp {randomMonster.Hp}");
+                Console.WriteLine($"Lv. {randomMonster.Lv} {randomMonster.Name} Hp {randomMonster.Hp}");
             }
             Console.WriteLine("\n");
             Console.WriteLine("[내정보]");
-            //Console.WriteLine(Player.BattleState); //변수로 플레이어도 객체로 있어야한다.?
+            player.PrintStatus();
             Console.WriteLine("");
             Console.WriteLine("1. 공격");
             Console.WriteLine("");
-            Console.WriteLine("원하시는 행동을 입력해주세요.");
-            Console.Write(">> ");
-
-            //// 버튼 메서드를 만들었을 때?
-            //switch ()
-            //{
-            //    case 1:
-            //        ChoiceMonster();
-            //        break;
-            //}
         }
 
         private void ChoiceMonster()
         {
-            Console.Clear ();
-
-            Console.WriteLine("Battle!");
-            Console.WriteLine("");
-            for (int i = 0; i < 3; i++)
+            while(true)
             {
-                Monster randomMonster = GetRandomMonster(monsters);
-                Console.WriteLine($"-{i + 1} Lv. {randomMonster.Lv} {randomMonster.Name} Hp {randomMonster.Hp}");
-            }
-            Console.WriteLine("\n");
-            Console.WriteLine("[내정보]");
-            //Console.WriteLine(Player.BattleState);
-            Console.WriteLine("");
-            Console.WriteLine("0. 취소");
-            Console.WriteLine("");
-            Console.WriteLine("대상을 선택해주세요.");
-            Console.WriteLine(">> ");
+                Console.Clear();
 
-            //switch (Choice(0, 3))
-            //{
-            //    case 0;
-            //        Battle();
-            //        break;
-            //    case 1:
-            //        Player.Attack();
-            //        break;
-            //    case 2:
-            //        Player.Attack();
-            //        break;
-            //    case 3:
-            //        Player.Attack();
-            //        break;
-            //    default:
-            //        break;
-            //}
+                Console.WriteLine("Battle!");
+                Console.WriteLine("");
+                for (int i = 0; i < 3; i++)
+                {
+                    Monster randomMonster = GetRandomMonster(monsters);
+                    Console.WriteLine($"-{i + 1} Lv. {randomMonster.Lv} {randomMonster.Name} Hp {randomMonster.Hp}");
+                }
+                Console.WriteLine("\n");
+                Console.WriteLine("[내정보]");
+                player.PrintStatus();
+                Console.WriteLine("");
+                Console.WriteLine("0. 취소");
+                Console.WriteLine("");
+                Console.WriteLine("대상을 선택해주세요.");
+                Console.WriteLine(">> ");
+
+                //switch (MenuChoice("원하시는 행동을 입력해주세요.", 0, 3))
+                //{
+                //    case 0:
+                //        // 턴넘기기
+                //        break;
+                //    case 1:
+                //        player.Attack;
+                //        break;
+                //    case 2:
+                //        player.Attack;
+                //        break;
+                //    case 3:
+                //        player.Attack;
+                //        break;
+                //    default:
+                //        break;
+                //}
+            }           
         }
 
         static Monster GetRandomMonster(List<Monster> monsters)
@@ -90,21 +101,6 @@ namespace TeamProject2
             Random random = new Random();
             int index = random.Next(monsters.Count);
             return monsters[index];
-        }
-    }
-
-    internal class Monster // GameObjects의 Monsters.cs로 가야함 (그리고 using TeamProject2.GameObjects.Monster 추가)
-    {
-        public int Lv { get; }
-        public string Name { get; }
-        public int Hp { get; set; }
-
-
-        public Monster(int lv, string name, int hp)
-        {
-            Lv = lv;
-            Name = name;
-            Hp = hp;
         }
     }
 }
