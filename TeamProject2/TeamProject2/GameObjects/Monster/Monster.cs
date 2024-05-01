166731d1bd2513ed9738f150a5b7a00d36d510fd
chaerin1101 <chaerin1101@naver.com>
2024-05-01 11:47:39 +0900

    fix namespace

--- TeamProject2/TeamProject2/GameObjects/Monster/Monster.cs
+++ TeamProject2/TeamProject2/GameObjects/Monster/Monster.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TeamProject2.GameObjects.Monster
+namespace TeamProject2
 {
     internal class Monster
     {
--- TeamProject2/TeamProject2/Scenes/MainScene.cs
+++ TeamProject2/TeamProject2/Scenes/MainScene.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TeamProject2.Scenes
+namespace TeamProject2
 {
     internal class MainScene
     {
