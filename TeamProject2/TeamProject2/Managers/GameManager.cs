a92282fecc36c3d8465fbecf2723e20581f112ff
jhj603 <82034869+jhj603@users.noreply.github.com>
2024-05-01 21:12:36 +0900

    [fix]CreateStatus

--- TeamProject2/TeamProject2/Managers/GameManager.cs
+++ TeamProject2/TeamProject2/Managers/GameManager.cs
@@ -8,8 +8,6 @@ namespace TeamProject2
 {
     internal class GameManager
     {
-        //Player Player = new Player(10, 100, 5, 2, "육회", "전사", 1500);
-
         Player status = null;               // 플레이어 객체
         BattleScene battleScene = null;     // 배틀씬 객체
 
@@ -18,7 +16,7 @@ namespace TeamProject2
 
             if(status == null)  // status == null이면
             {
-                status = new Player();    // Player 객체 생성
+                status = new Player(10, 100, 5, 2, "육회", "전사", 1500);    // Player 객체 생성
             }
 
             while (true)    // 무한 반복
@@ -60,7 +58,7 @@ namespace TeamProject2
                 Console.WriteLine("상태 보기");                        // 상태 보기 화면("캐릭터의 정보가 표시됩니다." 까지) 출력
                 Console.WriteLine("캐릭터의 정보가 표시됩니다.\n");
 
-                Player.ShowStatus();                                   // Player.ShowStatus() 호출
+                status.ShowStatus();                                   // Player.ShowStatus() 호출
 
                 Console.WriteLine("\n0. 나가기\n");                    // 상태 보기 화면(나머지) 출력
                 Console.WriteLine("원하시는 행동을 입력해주세요.");
