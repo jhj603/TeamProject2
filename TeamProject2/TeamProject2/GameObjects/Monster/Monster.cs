2ec15b5077a49b168c4eb097d467054bf2ebdb40
engur88 <thsengur88@naver.com>
2024-05-02 10:56:48 +0900

    [feat]ShowMonsterStatus

--- TeamProject2/TeamProject2/GameObjects/Monster/Monster.cs
+++ TeamProject2/TeamProject2/GameObjects/Monster/Monster.cs
@@ -11,13 +11,25 @@ namespace TeamProject2
         // private으로 접근 제한해놨으므로
         // Monster 클래스 외부에서 사용하려면 프로퍼티를 적용해야 함
 
-        private int attack = 0;
+        //private int attack = 0;
 
-        private int hp = 0;
+        //private int hp = 0;
 
-        private int level = 0;
+        //private int level = 0;
 
-        private string name = null;
+        //private string name = null;
+        public int Attack { get; set; }
+        public int Hp { get; set; }
+        public int Level { get; set; }
+        public string Name { get; set; }
+
+        public Monster(int attack, int hp, int level, string name)
+        {
+            Attack = attack;
+            Hp = hp;
+            Level = level;
+            Name = name;
+        }
 
         void MonsterAttack(Player player)
         {
@@ -30,6 +42,7 @@ namespace TeamProject2
         void ShowMonsterStatus()
         {
             // 레벨, 이름, hp로 문자열 출력
+            Console.WriteLine($"Lv.{Level} {Name}  HP {Hp}");
         }
     }
 }
