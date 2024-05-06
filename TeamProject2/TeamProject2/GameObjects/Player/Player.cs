c836f39b4dff2de596e0c36e448dfc4c2d25c2c3
Changil Cho <chotogi98@gmail.com>
2024-05-06 14:44:38 +0900

    Update Player.cs

--- TeamProject2/TeamProject2/GameObjects/Player/Player.cs
+++ TeamProject2/TeamProject2/GameObjects/Player/Player.cs
@@ -34,7 +34,7 @@ namespace TeamProject2
         {
             get { return random_attackErrorrange; }
         }
-    
+
         private int attack = 0;            // 공격력
 
         private int hp = 0;                // HP
@@ -48,12 +48,12 @@ namespace TeamProject2
         private string job = null;         // 직업
 
         private int gold = 0;              // 소지금
-        
+
         private int maxhp = 0;             // 최대 HP
 
         private int random_attackErrorrange = 0;
 
-        public Player (int attack, int hp, int defense, int level, string name, string job, int gold)
+        public Player(int attack, int hp, int defense, int level, string name, string job, int gold)
         {
             this.attack = attack;
             this.hp = hp;
@@ -68,13 +68,32 @@ namespace TeamProject2
         public void ShowStatus()
         {
             // 플레이어 필드를 사용해 문자열 생성 후 출력
-            Console.WriteLine($"Lv. {0:D1}{level}");            
-            Console.WriteLine($"{name} ( {job} )");
-            Console.WriteLine($"공격력 : {attack}");
-            Console.WriteLine($"방어력 : {defense}");
-            Console.WriteLine($"체  력 : {hp}");
-            Console.WriteLine($"Gold : {gold} G");
-            Console.Write(ConsoleUtility.PadRightForMixedText("Gold", 12));
+            Console.WriteLine($"Lv. {0:D1}{level}");
+
+            Console.Write($"{"닉네임",-3} : ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"{name}");
+            Console.ResetColor();
+            Console.WriteLine($" ( {job} )\n");
+
+            Console.WriteLine($"{"공격력", -3} : {attack}");
+            Console.WriteLine($"{"방어력", -3} : {defense}");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"{"체  력", -3}");
+            Console.ResetColor();
+            Console.Write(" : ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{hp}");
+            Console.ResetColor();
+
+            Console.ForegroundColor= ConsoleColor.DarkYellow;
+            Console.Write($"{"소지금", -3}");
+            Console.ResetColor();
+            Console.Write(" : ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"{gold} G");
+            Console.ResetColor();
         }
 
         public void PlayerAttack(Monster monster)
